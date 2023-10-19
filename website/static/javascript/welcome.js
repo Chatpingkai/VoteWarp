70e32655fbb9c2f7e06b2f054dbd174fd6f2686f
Chatpingkai <i0650566653@gmail.com>
2023-10-19 19:19:52 +0700

    welcome transition 6%

--- website/static/javascript/welcome.js
+++ website/static/javascript/welcome.js
@@ -2,3 +2,13 @@ let vid = document.getElementById("planevideo")
 function playvid() {
     vid.play()
 }
+
+function cloud_transition() {
+    const cloudimation = document.getElementById("cloudimation");
+    cloudimation.style.display = "block";
+    cloudimation.style.animation = "cloud 3s";
+    setTimeout(waitnewpage, 3000)
+}
+function waitnewpage() {
+    window.location.href = "login.html";
+}
