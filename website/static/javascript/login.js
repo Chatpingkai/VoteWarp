dc357f243f6469302e88cd8ef72301ead32f002c
Chatpingkai <i0650566653@gmail.com>
2023-10-23 22:52:10 +0700

    เพิ่ม transition ใครไม่ชอบ ผมไม่รู้

---
+++ website/static/javascript/login.js
@@ -0,0 +1,5 @@
+function cloud_transition() {
+    const cloudimation = document.getElementById("cloudimation");
+    cloudimation.style.display = "block";
+    cloudimation.style.animation = "cloud 3s";
+}
--- website/static/javascript/welcome.js
+++ website/static/javascript/welcome.js
@@ -3,12 +3,21 @@ function playvid() {
     vid.play()
 }
 
-function cloud_transition() {
+function cloud_transition_login() {
     const cloudimation = document.getElementById("cloudimation");
     cloudimation.style.display = "block";
     cloudimation.style.animation = "cloud 3s";
-    setTimeout(waitnewpage, 3000)
+    setTimeout(wait_login, 2500)
 }
-function waitnewpage() {
+function wait_login() {
     window.location.href = "login.html";
+}
+function cloud_transition_register() {
+    const cloudimation = document.getElementById("cloudimation");
+    cloudimation.style.display = "block";
+    cloudimation.style.animation = "cloud 3s";
+    setTimeout(wait_register, 2500)
+}
+function wait_register() {
+    window.location.href = "Register.html";
 }
