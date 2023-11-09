a1446a2a34f8ae041ad59720b7cfe842d7614d4a
Chatpingkai <i0650566653@gmail.com>
2023-11-09 21:52:54 +0700

    check button

--- website/static/javascript/room.js
+++ website/static/javascript/room.js
@@ -1,3 +1,6 @@
-function checked() {
-    document.getElementById("checkbyn")
-}
+const check = document.querySelectorAll(".confirm");
+check.forEach(ho => {
+    ho.addEventListener("click", function() {
+        ho.classList.toggle("checked")
+    })
+})
